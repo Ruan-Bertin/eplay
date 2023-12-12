7e7130cd75d15b67eb2a7ad87a38549ccdebaef9
Ruan Bertin <ruanbertin@hotmail.com>
2023-12-12 07:47:20 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg'
+
+import { GlobalCss } from './styles'
+
+function App() {
+  return (
+    <>
+      <GlobalCss />
+      <div>olá mundo</div>
+    </>
+  )
+}
+
+export default App
