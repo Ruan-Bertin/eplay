d0ceb8a7454ac7033925a3a879658e04dcaab99e
Ruan Bertin <ruanbertin@hotmail.com>
2023-12-16 04:54:47 -0300

    Feat: criando os componentes Button e Tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+
+// Criamos uma constante chamada rotas e utilizamos a função createBrowserRouter, essa função vai receber um array de objetos.
+const rotas = createBrowserRouter([
+  {
+    // em path colocamos o caminho inicial, que no caso é a barra'/'
+    path: '/',
+    // em element colocamos o que será renderizado, nesse caso é só o banner por enquanto
+    element: <Banner />
+  }
+])
 
 function App() {
   return (
@@ -9,7 +21,7 @@ function App() {
       <div className="container">
         <Header />
       </div>
-      <Banner />
+      <RouterProvider router={rotas} />
     </>
   )
 }
